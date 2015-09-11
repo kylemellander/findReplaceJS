a654283151373e6c86b8b4177bf8ee843e3559ee
Kyle Mellander <kylemellander@gmail.com>
2015-09-11 06:52:11 -0700

    Update code to replace multiple instances

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 function findReplace(phrase, oldWord, newWord) {
-  return phrase.replace(oldWord, newWord);
+  return phrase.replace(new RegExp(oldWord, 'g'), newWord);
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("findReplace", function() {
   it("will find a one word phrase and replace it with a new word", function() {
     expect(findReplace("word", "word", "awesome")).to.equal("awesome")
   })
+
+  it("will find many words in a sentence and replace it with a new word", function() {
+    expect(findReplace("How much wood would you trade for more wood?", "wood", "steak")).to.equal("How much steak would you trade for more steak?")
+  })
 })
