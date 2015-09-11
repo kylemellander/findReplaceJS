5dcc485b124cdabac9685f7bb7057175c8bace33
Kyle Mellander <kylemellander@gmail.com>
2015-09-11 07:25:12 -0700

    Add frontend for getting information and returning result

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 function findReplace(phrase, oldWord, newWord) {
   return phrase.replace(new RegExp(oldWord.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), newWord);
 }
+
+$(document).ready(function() {
+  $("form#findReplace").submit(function(event) {
+    var phrase = $("#phrase").val();
+    var oldWord = $("#oldWord").val();
+    var newWord = $("#newWord").val();
+    var result = findReplace(phrase, oldWord, newWord);
+
+    $("#results p").text(result);
+    $("#results").show();
+
+    event.preventDefault();
+  })
+})
